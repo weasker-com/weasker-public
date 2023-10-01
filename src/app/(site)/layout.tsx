71867a7fe3a37693470b89b170aa54a425799331
omribarmats <omri.bar@gmail.com>
2023-10-01 16:28:55 +0300

    Added Vercel analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         </header>
         <main className="flex min-h-screen flex-col gap-8 items-center w-11/12 sm:max-w-7xl mx-auto">
           {children}
+          <Analytics />
         </main>
         <footer>
           <Footer />
