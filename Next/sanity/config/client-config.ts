64622c72c75c140b9bc67192e9d819a737163d7b
omribarmats <omri.bar@gmail.com>
2023-12-25 12:33:01 +0200

    removed sanity folder

--- Next/sanity/config/client-config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  projectId: "86a07a92",
-  dataset: "production",
-  apiVersion: "2023-09-06",
-};
-
-export default config;
--- Next/sanity/schemas/miniSchemas/excerpt-Schema.tsx
+++
@@ -1,8 +0,0 @@
-import { RuleType } from "../../../types/rule-type";
-
-export const excerptSchema = {
-  name: "excerpt",
-  title: "Excerpt",
-  type: "string",
-  validation: (Rule: RuleType) => Rule.required(),
-};
