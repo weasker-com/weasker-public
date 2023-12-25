c4a7a9bcb77ee917ed497d2372ba2aa761a66560
omribarmats <omri.bar@gmail.com>
2023-12-25 12:03:43 +0200

    Removed  unused types

--- Next/types/badge-type.tsx
+++
@@ -1,6 +0,0 @@
-export type badge = {
-  name: string;
-  singularName: string;
-  image: string;
-  slug: string;
-};
--- Next/types/listItem-type.tsx
+++
@@ -1,9 +0,0 @@
-import { badge } from "./badge-type";
-
-export type listItem = {
-  title: string;
-  image: string;
-  slug: string;
-  badge: badge;
-  interviewSlug: string;
-};
