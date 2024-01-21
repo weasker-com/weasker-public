dcb91a684c32c00beef5b8c456111003c3016e45
omribarmats <omri.bar@gmail.com>
2024-01-21 13:51:25 +0200

    Layout: minor changes

--- Next/src/app/(site)/layout.tsx
+++ Next/src/app/(site)/layout.tsx
@@ -1,22 +1,18 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Noto_Sans } from "next/font/google";
 import Footer from "@/components/Footer";
 import { defaultImages } from "@/utils/defaultImages";
 import PlausibleProvider from "next-plausible";
 import { AuthProvider } from "../../providers/Auth/Auth";
 
-
-const inter = Inter({ subsets: ["latin"] });
 const noto_Sans = Noto_Sans({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const ogImage = `${process.env.SITE_URL}/api/og?img=${defaultImages.weaskerLogoUrl}&preTitle=Interviewing experts&title=weasker.com`;
-
 const metaTitle = `${process.env.SITE_NAME} - Interviewing Experts`;
 const metaDescription =
   "We interview groups of experts and compare their answers, generating diverse and reliable information sources.";
@@ -67,7 +63,6 @@ export default function RootLayout({
           </header>
           <main className="flex min-h-screen flex-col items-center">
             {children}
-            <Analytics />
           </main>
           <footer>
             <Footer />
