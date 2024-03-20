0b25d184ff8dd0efdee2e22d8651bc05210649b0
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2024-03-20 17:01:29 +0100

    InterviewAnswer: Persist scroll position when changing user

--- Next/src/components/InterviewAnswer.tsx
+++ Next/src/components/InterviewAnswer.tsx
@@ -11,6 +11,23 @@ import { WideBox } from "./ui/boxes";
 import Answer from "./Answer";
 import { defaultImages } from "../utils/defaultImages";
 import { InternalLink } from "./links/InternalLink";
+import { useCallback, useLayoutEffect, useRef } from "react";
+
+function getElementOffset(el: HTMLElement | Element | null) {
+  let left = 0;
+  let top = 0;
+
+  while (
+    el instanceof HTMLElement &&
+    !isNaN(el.offsetLeft) &&
+    !isNaN(el.offsetTop)
+  ) {
+    left += el.offsetLeft - el.scrollLeft;
+    top += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { top: top, left: left };
+}
 
 export type InterviewAnswerProps = {
   badgeSlug: string;
@@ -42,14 +59,54 @@ const InterviewAnswer = ({
   onGoToNextUser,
   onContactUser,
 }: InterviewAnswerProps) => {
+  // Keep a ref to the container so we can measure its position on the page
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lastScrollPositionRef = useRef<number | undefined>(undefined);
+  const lastContainerPositionRef = useRef<number | undefined>(undefined);
+  const userChangedAtRef = useRef<number | undefined>(undefined);
+
   const currentAnswer =
     relevantAnswers.filter(
       (answer) => answer.user.seo.slug === currentUser.seo.slug
     )[0] || relevantAnswers[0];
 
+  // Every time before we change the user, we want to save the scroll position
+  // before doing so, so that we can account for the layout shift that happens
+  // after all InterviewAnswers rerender
+  const savelastScrollPosition = useCallback(() => {
+    lastScrollPositionRef.current = document.documentElement.scrollTop;
+    lastContainerPositionRef.current = getElementOffset(
+      containerRef.current
+    ).top;
+    userChangedAtRef.current = Date.now();
+  }, []);
+
+  useLayoutEffect(() => {
+    const lastScrollPosition = lastScrollPositionRef.current;
+    const lastScrollPositionSavedAt = userChangedAtRef.current;
+    const lastContainerPosition = lastContainerPositionRef.current;
+    if (
+      typeof lastScrollPosition !== "number" ||
+      typeof lastScrollPositionSavedAt !== "number" ||
+      typeof lastContainerPosition !== "number"
+    ) {
+      return;
+    }
+
+    // If the user changed than 300ms ago, we don't want to adjust the scroll position
+    if (Date.now() - lastScrollPositionSavedAt > 300) {
+      return;
+    }
+
+    const currentContainerPosition = getElementOffset(containerRef.current).top;
+
+    document.documentElement.scrollTop =
+      lastScrollPosition + currentContainerPosition - lastContainerPosition;
+  }, [currentUser]);
+
   return (
     <WideBox className="p-5" id={question.question.seo.slug}>
-      <div className="flex flex-col gap-5 w-full">
+      <div ref={containerRef} className="flex flex-col gap-5 w-full">
         <div>
           <ImageAndText
             number={questionNumber}
@@ -58,13 +115,23 @@ const InterviewAnswer = ({
         </div>
         {relevantAnswers.length > 1 && (
           <div className="relative flex items-center w-full">
-            <div onClick={onGoToPrevUser}>{leftArrowIcon(20)}</div>
+            <div
+              onClick={() => {
+                savelastScrollPosition();
+                onGoToPrevUser();
+              }}
+            >
+              {leftArrowIcon(20)}
+            </div>
             <div className="flex overflow-x-scroll scrollbar-hide w-full no-scrollbar">
               <div className="flex space-x-2">
                 {relevantAnswers.map((item, index) => (
                   <div
                     key={index}
-                    onClick={() => onChangeUser(item.user)}
+                    onClick={() => {
+                      savelastScrollPosition();
+                      onChangeUser(item.user);
+                    }}
                     className="flex-shrink-0"
                   >
                     <ImageAndText
@@ -77,7 +144,13 @@ const InterviewAnswer = ({
                 ))}
               </div>
             </div>
-            <div className="hover:cursor-pointer" onClick={onGoToNextUser}>
+            <div
+              className="hover:cursor-pointer"
+              onClick={() => {
+                savelastScrollPosition();
+                onGoToNextUser();
+              }}
+            >
               {rightArrowIcon(20)}
             </div>
           </div>
